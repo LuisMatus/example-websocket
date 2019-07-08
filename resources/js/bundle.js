08dd34a74f3d93d2579689a4857d7d0711e678de
Luis Matus <luismsl29@live.com.mx>
2019-07-08 04:06:50 -0500

    change port

--- resources/js/bundle.js
+++ resources/js/bundle.js
@@ -18,7 +18,7 @@ window.Vue = vue;
 Vue.use(VueRouter);
 Vue.use(Vuetify);
 
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:5000');
 socket.on('news', function (data) {
 	console.log(data);
 	socket.emit('my other event', { my: 'data' });
